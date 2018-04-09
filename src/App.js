15ad4a235469f961e08dcc4caefeb4c3903b1c3d
nico24687 <theonlynico@hotmail.com>
2018-04-09 16:26:07 -0600

    button changes from edit to save

--- src/App.js
+++ src/App.js
@@ -45,9 +45,27 @@ class App extends Component {
     this.toggleGuestPropertyAt("isEditing", index)
   }
 
+  setNameAt = (name, indexToChange) => {
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest,
+            name
+          }
+        }
+        return guest
+      })
+    })
+  }
+
+
+
   getTotalInvited = () => {
     this.state.guests.length
   }
+
+  
 
   //getAttendingGuests = () => 
   //getUnconfirmedGuests = () => 
@@ -89,7 +107,8 @@ class App extends Component {
         <GuestList
           guests={this.state.guests} 
           toggleConfirmationAt={this.toggleConfirmationAt}
-          toggleEditingAt={this.toggleEditingAt}  />
+          toggleEditingAt={this.toggleEditingAt}
+          setNameAt={this.setNameAt}  />
       </div>
     </div>
     );
